846d1255df6463b5b6c880fe6687ce674195f582
Ata <agastyo2004@gmail.com>
2023-02-15 10:03:48 +0700

    add dynemic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem() {
+  const expenseDate = new Date(2023, 2, 15);
+  const expenseTitle = "Car Insurance";
+  const expensePrice = "294.67";
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
       </div>
     </div>
   );
