82e79f1a32eac9e1e67128fe88c2d925a24f31bd
Ata <agastyo2004@gmail.com>
2023-02-15 16:18:53 +0700

    add event click

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -4,6 +4,9 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 export default function ExpenseItem(props) {
+  const clickHere = () => {
+    console.log("clicked");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -11,6 +14,7 @@ export default function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHere}>Click Here!</button>
     </Card>
   );
 }
